de67c1ae167474fb7c2c27164f57cf1ab0d7a312
Muhammad Reza <muhammadreza533@smk.belajar.id>
2022-01-31 14:14:00 +0700

    New Laravel Project Laundry

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,14 @@
+@extends('templates/header')
+@section('content')
+<section class="content">
+    <!-- Default box -->
+    <div class="card">
+      <div class="card-header">
+        
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+          </div>
+    </div>
+</section>
+@endsection
